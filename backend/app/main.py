3131469732af73c4e28da4ccdd5db46271983e8b
CierraBlackett <blackettcierra5@gmail.com>
2026-03-08 23:22:50 -0400

    Intial Schemas for Assignments, Students, and Courses created

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
-from app.routes import gpa_routes
+from routes import gpa_routes,assignment_routes, course_routes, student_routes
 
 app = FastAPI()
 
 app.include_router(gpa_routes.router)
+app.include_router(assignment_routes.router)
+app.include_router(course_routes.router)
+app.include_router(student_routes.router)
 
 @app.get("/")
+
 def root():
     return{"message": "PathGPA Backend Running"}
