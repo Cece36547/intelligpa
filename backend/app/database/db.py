e6f2c161a34af56890e07c78aca4078a4aeffa77
CierraBlackett <blackettcierra5@gmail.com>
2026-05-18 00:51:01 -0400

    Debug Railway DATABASE_URL

--- backend/app/database/db.py
+++ backend/app/database/db.py
@@ -13,6 +13,7 @@ from app.models.assignmentCategory import AssignmentCategory
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+print("DATABASE_URL =", DATABASE_URL)
 
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL environment variable is not set")
